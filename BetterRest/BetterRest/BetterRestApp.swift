4b4fed5eb7e3ffa8d45141a0a8e1915563cbea6d
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-14 18:46:31 +0400

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Abdurokhman Mukhanbetov on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
